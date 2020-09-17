fe3af23ffc81dda80e087864ac9b182f28fafff3
rishiporwal <64612539+rishiporwal@users.noreply.github.com>
2020-09-17 18:42:07 -0500

    All characters and level one complete

--- main.ts
+++ main.ts
@@ -2,9 +2,22 @@ namespace SpriteKind {
     export const Virtical = SpriteKind.create()
     export const Horizontal = SpriteKind.create()
 }
+function Colors2 () {
+    color.setColor(11, color.rgb(101, 101, 101))
+    color.setColor(9, color.rgb(0, 30, 225))
+    color.setColor(8, color.rgb(0, 30, 150))
+    color.setColor(10, color.rgb(78, 197, 0))
+    color.setColor(12, color.rgb(50, 50, 50))
+    color.setColor(5, color.rgb(220, 220, 7))
+    color.setColor(4, color.rgb(79, 70, 37))
+}
 controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
     moveV(-16)
 })
+function Make_Invisible (Vehicle: Sprite) {
+    Vehicle.setFlag(SpriteFlag.Ghost, true)
+    Vehicle.setFlag(SpriteFlag.Invisible, true)
+}
 controller.down.onEvent(ControllerButtonEvent.Released, function () {
     moveV(16)
 })
@@ -98,121 +111,368 @@ function moveV (direction: number) {
 controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
     MoveH(16)
 })
+function Put_in_level (Vehicle: Sprite, x: number, y: number) {
+    Vehicle.setFlag(SpriteFlag.Ghost, false)
+    Vehicle.setFlag(SpriteFlag.Invisible, false)
+}
+function Create_Charecters () {
+    Driver = sprites.create(img`
+        ........b.......
+        ....ff2222ff....
+        ..2ff222222ff2..
+        .22222222222222.
+        2222222222222222
+        2222222222222222
+        2222222222222222
+        2222222222222222
+        22222ffffff22222
+        222ffffffffff222
+        f2ffffffffffff2f
+        222ffffffffff222
+        2f2ffffffffff2f2
+        2f22ffffffff22f2
+        2ff2ffffffff2ff2
+        2ff22f2222f22ff2
+        2ff2222222222ff2
+        2ff2222222222ff2
+        2ff2222222222ff2
+        2fff22222222fff2
+        2fff22222222fff2
+        2fff22222222fff2
+        2fff22222222fff2
+        2fff22222222fff2
+        2ff22f2222f22ff2
+        2ff2ffffffff2ff2
+        2f22ffffffff22f2
+        2f2ffffffffff2f2
+        2f2ffffffffff2f2
+        222ffffffffff222
+        .22fff2222fff22.
+        ..222222222222..
+        `, SpriteKind.Virtical)
+    Driver.setPosition(72, 64)
+    Limo_V3 = sprites.create(img`
+        ........b.......
+        ....ffccccff....
+        ..cffccccccffc..
+        .cccccccccccccc.
+        cccccccccccccccc
+        cccccccccccccccc
+        cccccccccccccccc
+        cccccccccccccccc
+        cccccccccccccccc
+        cccccccccccccccc
+        cccccccccccccccc
+        cccccccccccccccc
+        cccccffffffccccc
+        cccffffffffffccc
+        fcffffffffffffcf
+        cccffffffffffccc
+        cfcffffffffffcfc
+        cfccffffffffccfc
+        cffcffffffffcffc
+        cffccfccccfccffc
+        cffccccccccccffc
+        cffccccccccccffc
+        cffccccccccccffc
+        cffccccccccccffc
+        cffccccccccccffc
+        cffccccccccccffc
+        cffccccccccccffc
+        cffccccccccccffc
+        cffccccccccccffc
+        cffccccccccccffc
+        cfffccccccccfffc
+        cfffccccccccfffc
+        ccffccccccccffcc
+        cfccccccccccccfc
+        cfffccccccccfffc
+        cfffccccccccfffc
+        cfffccccccccfffc
+        cfffccccccccfffc
+        cfffccccccccfffc
+        cfffccccccccfffc
+        cffccfccccfccffc
+        cffcffffffffcffc
+        cfccffffffffccfc
+        cfcffffffffffcfc
+        cfcffffffffffcfc
+        cccffffffffffccc
+        .ccfffccccfffcc.
+        ..cccccccccccc..
+        `, SpriteKind.Virtical)
+    Limo_V3.setPosition(88, 40)
+    TruckH3 = sprites.create(img`
+        ..55ffffffffffffffffffffffffffffffff5ff555555...
+        .55f55fffffffffffffffffffffffffffff5ffff555555..
+        55ff5555555555555555555555555555555ffffff555555.
+        5fff55ccccccccccccccccccccccccccc55ffffff55555f.
+        5fff5c555555555555555555555555555c5ffffffccc55f.
+        5fff5c555555555555555555555555555c5fffffff55ccc.
+        5fff5c555555555555555555555555555c5fffffff55555.
+        5fff5c555555555555555555555555555c5fffffff55555.
+        5fff5c555555555555555555555555555c5fffffff55555b
+        5fff5c555555555555555555555555555c5fffffff55555.
+        5fff5c555555555555555555555555555c5fffffff55ccc.
+        5fff5c555555555555555555555555555c5ffffffccc55f.
+        5fff55ccccccccccccccccccccccccccc55ffffff55555f.
+        55ff5555555555555555555555555555555ffffff555555.
+        .55f55fffffffffffffffffffffffffffff5ffff555555..
+        ..55ffffffffffffffffffffffffffffffff5ff555555...
+        `, SpriteKind.Horizontal)
+    TruckH3.setPosition(56, 40)
+    JeepV2 = sprites.create(img`
+        ........b.......
+        ....eeeeeeee....
+        ...ee444444ee...
+        ..eff444444ffe..
+        .eeee4eeee4eeee.
+        .eeee4eeee4eeee.
+        deeee4eeee4eeeed
+        .eeee4eeee4eeee.
+        feeee4eeee4eeeef
+        feee44444444eeef
+        feee4ffffff4eeef
+        feeffffffffffeef
+        feffffffffffffef
+        .effffffffffffe.
+        .effffffffffffe.
+        .efe44444444efe.
+        .eef44444444fee.
+        .eff44444444ffe.
+        44ff44444444ff44
+        e4ff44444444ff4e
+        .4ff44444444ff4.
+        .efe44444444efe.
+        .eeeffffffffeee.
+        .effffffffffffe.
+        .e4e44444444e4e.
+        fe4eeeeeeeeee4ef
+        fe444444444444ef
+        fe444444444444ef
+        fe444444444444ef
+        fe444444444444ef
+        .ee4444444444ee.
+        ..eeeeeeeeeeee..
+        `, SpriteKind.Virtical)
+    JeepV2.setPosition(56, 64)
+    JeepH2 = sprites.create(img`
+        ..fffff.....dc.....fffff........
+        .ddddddddddcccdddddddddddddd....
+        ddccccccfdfffffdffffddddddddd...
+        dcccccddfddfffffdffffdddddddfd..
+        dcccccdcffcccccccffffccdddddfdd.
+        dcccccdcffcccccccfffffccccccccd.
+        dcccccdcffcccccccfffffcdddddccd.
+        dcccccdcffcccccccfffffcdddddccd.
+        dcccccdcffcccccccfffffcdddddccdb
+        dcccccdcffcccccccfffffcdddddccd.
+        dcccccdcffcccccccfffffccccccccd.
+        dcccccdcffcccccccffffccdddddfdd.
+        dcccccddfddfffffdffffdddddddfd..
+        ddccccccfdfffffdffffddddddddd...
+        .ddddddddddcccdddddddddddddd....
+        ..fffff.....dc.....fffff........
+        `, SpriteKind.Horizontal)
+    JeepH2.setPosition(96, 72)
+    TruckV3 = sprites.create(img`
+        ........b.......
+        ..7ffeeeeeeff7..
+        .7777eaaaae7777.
+        77777eaaaae77777
+        7777eaaaaaae7777
+        7777eaaaaaae7777
+        7777effffffe7777
+        77ffffffffffff77
+        7ffffffffffffff7
+        ffffffffffffffff
+        ffffffffffffffff
+        7ffffffffffffff7
+        f7ffffffffffff7f
+        ff777777777777ff
+        ff777777777777ff
+        f77777777777777f
+        ff777777777777ff
+        ff777eeeeee777ff
+        f777eaaaaaae777f
+        ff77eaaaaaae77ff
+        ff77eaaaaaae77ff
+        f777eaaaaaae777f
+        ff77eaaaaaae77ff
+        ff77eaaaaaae77ff
+        f777eaaaaaae777f
+        ff77eaaaaaae77ff
+        ff77eaaaaaae77ff
+        f777eaaaaaae777f
+        ff77eaaaaaae77ff
+        ff77eaaaaaae77ff
+        f777eaaaaaae777f
+        ff77eaaaaaae77ff
+        ff77eaaaaaae77ff
+        f777eaaaaaae777f
+        ff77eaaaaaae77ff
+        ff77eaaaaaae77ff
+        f777eaaaaaae777f
+        ff77eaaaaaae77ff
+        ff77eaaaaaae77ff
+        f777eaaaaaae777f
+        ff777eeeeee777ff
+        ff777777777777ff
+        f77777777777777f
+        f77777777777777f
+        effffffffffffffe
+        eeffffffffffffee
+        .eeffffffffffee.
+        ..eeeeeeeeeeee..
+        `, SpriteKind.Virtical)
+    TruckV3.setPosition(40, 72)
+    PartyBusH3 = sprites.create(img`
+        ..99ffffffffffffffffffffffffffffffff9ff999999...
+        .99f99fffffffffffffffffffffffffffff9ffff999999..
+        99ff9999999999999999999999999999999ffffff999999.
+        9fff999999999ccccc99999999ddddd9999ffffff99999f.
+        9fff999999999111cc99dd99ddddfddd999ffffff66699f.
+        9fff9999999991ffcc9dddddddfddddd999fffffff99666.
+        9fff9999f9f99111cc9dddddccddfdddd99fffffff99999.
+        9fff999ccccff111ccfffffcffcdfdddd99fffffff99999.
+        9fff999ccccff1ffccfffffcffcdfdddd99fffffff99999b
+        9fff9999f9f99111cc9dddddccddfdddd99fffffff99999.
+        9fff9999999991ffcc9ddddddddddddd999fffffff99666.
+        9fff999999999111cc99dd99dddddddd999ffffff66699f.
+        9fff999999999ccccc99999999ddddd9999ffffff99999f.
+        99ff9999999999999999999999999999999ffffff999999.
+        .99f99fffffffffffffffffffffffffffff9ffff999999..
+        ..99ffffffffffffffffffffffffffffffff9ff999999...
+        `, SpriteKind.Horizontal)
+    PartyBusH3.setPosition(104, 88)
+    TaxiV2 = sprites.create(img`
+        ........b.......
+        ....cc5555cc....
+        ..555555555555..
+        .55555555555555.
+        5555555555555555
+        5555555555555555
+        5555555555555555
+        5555555555555555
+        55555ffffff55555
+        555ffffffffff555
+        c5ffffffffffff5c
+        555ffffffffff555
+        5f5ffffffffff5f5
+        5f55ffffffff55f5
+        5ff5555555555ff5
+        5ff5555555555ff5
+        5ff5555555555ff5
+        5ff5555555555ff5
+        5ff5555ff5555ff5
+        5fff5ccffcc5fff5
+        5fff555ff555fff5
+        5fff55555555fff5
+        5fff55555555fff5
+        5fff55555555fff5
+        5ff5555555555ff5
+        5ff5ffffffff5ff5
+        5f55ffffffff55f5
+        5f5ffffffffff5f5
+        5f5ffffffffff5f5
+        555ffffffffff555
+        .55fff5555fff55.
+        ..555555555555..
+        `, SpriteKind.Virtical)
+    TaxiV2.setPosition(56, 96)
+    TaxiH2 = sprites.create(img`
+        ..555555555555555555ff555555....
+        .55ffffffffffffffff5555555555...
+        555555fffffffffff5555f55555555..
+        5ffff555fffff555555ffff555555f..
+        5ffffff55555555555fffff555555ff.
+        5ffffff55555f55555ffffff555555f.
+        55fffff55555f55555ffffff5555555.
+        555fffff55ffff555ffffffff555555.
+        555fffff55ffff555ffffffff555555b
+        55fffff55555f55555ffffff5555555.
+        5ffffff55555f55555ffffff555555f.
+        5ffffff55555555555fffff555555ff.
+        5ffff555fffff555555ffff555555f..
+        555555fffffffffff5555f55555555..
+        .55ffffffffffffffff5555555555...
+        ..555555555555555555ff555555....
+        `, SpriteKind.Horizontal)
+    TaxiH2.setPosition(96, 104)
+    CarH2 = sprites.create(img`
+        ..888888888888888888ff888888....
+        .88ffffffffffffffff8888888888...
+        888888fffffffffff8888f88888888..
+        8ffff888fffff888888ffff888888f..
+        8ffffff88888888888fffff888888ff.
+        8ffffff88888888888ffffff888888f.
+        88fffff88888888888ffffff8888888.
+        88ffffff888888888ffffffff888888.
+        88ffffff888888888ffffffff888888b
+        88fffff88888888888ffffff8888888.
+        8ffffff88888888888ffffff888888f.
+        8ffffff88888888888fffff888888ff.
+        8ffff888fffff888888ffff888888f..
+        888888fffffffffff8888f88888888..
+        .88ffffffffffffffff8888888888...
+        ..888888888888888888ff888888....
+        `, SpriteKind.Horizontal)
+    CarH2.setPosition(112, 56)
+    CarV2 = sprites.create(img`
+        ........b.......
+        ....cc3333cc....
+        ..333333333333..
+        .33333333333333.
+        3333333333333333
+        3333333333333333
+        3333333333333333
+        3333333333333333
+        33333ffffff33333
+        333ffffffffff333
+        c3ffffffffffff3c
+        333ffffffffff333
+        3f3ffffffffff3f3
+        3f33ffffffff33f3
+        3ff3333333333ff3
+        3ff3ffffffff3ff3
+        3ff3ffffffff3ff3
+        3ff33ffffff33ff3
+        3ff3333333333ff3
+        3fff33333333fff3
+        3fff33333333fff3
+        3fff33333333fff3
+        3fff33333333fff3
+        3fff33333333fff3
+        3ff3333333333ff3
+        3ff3ffffffff3ff3
+        3f33ffffffff33f3
+        3f3ffffffffff3f3
+        3f3ffffffffff3f3
+        333ffffffffff333
+        .33fff3333fff33.
+        ..333333333333..
+        `, SpriteKind.Virtical)
+    CarV2.setPosition(120, 32)
+}
 let overlap = false
+let CarV2: Sprite = null
+let CarH2: Sprite = null
+let TaxiV2: Sprite = null
+let TaxiH2: Sprite = null
+let PartyBusH3: Sprite = null
+let TruckV3: Sprite = null
+let JeepH2: Sprite = null
+let JeepV2: Sprite = null
+let Limo_V3: Sprite = null
+let TruckH3: Sprite = null
+let Driver: Sprite = null
 let H_list: Sprite[] = []
 let V_list: Sprite[] = []
-color.setColor(11, color.rgb(101, 101, 101))
-color.setColor(9, color.rgb(0, 30, 225))
-color.setColor(8, color.rgb(0, 30, 150))
-color.setColor(10, color.rgb(78, 197, 0))
-color.setColor(12, color.rgb(50, 50, 50))
+let Level = 1
+Colors2()
 scene.setBackgroundColor(11)
 tiles.setTilemap(tilemap`level_0`)
-let Driver = sprites.create(img`
-    ........b.......
-    ....ff2222ff....
-    ..2ff222222ff2..
-    .22222222222222.
-    2222222222222222
-    2222222222222222
-    2222222222222222
-    2222222222222222
-    22222ffffff22222
-    222ffffffffff222
-    f2ffffffffffff2f
-    222ffffffffff222
-    2f2ffffffffff2f2
-    2f22ffffffff22f2
-    2ff2ffffffff2ff2
-    2ff22f2222f22ff2
-    2ff2222222222ff2
-    2ff2222222222ff2
-    2ff2222222222ff2
-    2fff22222222fff2
-    2fff22222222fff2
-    2fff22222222fff2
-    2fff22222222fff2
-    2fff22222222fff2
-    2ff22f2222f22ff2
-    2ff2ffffffff2ff2
-    2f22ffffffff22f2
-    2f2ffffffffff2f2
-    2f2ffffffffff2f2
-    222ffffffffff222
-    .22fff2222fff22.
-    ..222222222222..
-    `, SpriteKind.Virtical)
-Driver.setPosition(72, 64)
-let Limo_V3 = sprites.create(img`
-    ........b.......
-    ....ffccccff....
-    ..cffccccccffc..
-    .cccccccccccccc.
-    cccccccccccccccc
-    cccccccccccccccc
-    cccccccccccccccc
-    cccccccccccccccc
-    cccccccccccccccc
-    cccccccccccccccc
-    cccccccccccccccc
-    cccccccccccccccc
-    cccccffffffccccc
-    cccffffffffffccc
-    fcffffffffffffcf
-    cccffffffffffccc
-    cfcffffffffffcfc
-    cfccffffffffccfc
-    cffcffffffffcffc
-    cffccfccccfccffc
-    cffccccccccccffc
-    cffccccccccccffc
-    cffccccccccccffc
-    cffccccccccccffc
-    cffccccccccccffc
-    cffccccccccccffc
-    cffccccccccccffc
-    cffccccccccccffc
-    cffccccccccccffc
-    cffccccccccccffc
-    cfffccccccccfffc
-    cfffccccccccfffc
-    ccffccccccccffcc
-    cfccccccccccccfc
-    cfffccccccccfffc
-    cfffccccccccfffc
-    cfffccccccccfffc
-    cfffccccccccfffc
-    cfffccccccccfffc
-    cfffccccccccfffc
-    cffccfccccfccffc
-    cffcffffffffcffc
-    cfccffffffffccfc
-    cfcffffffffffcfc
-    cfcffffffffffcfc
-    cccffffffffffccc
-    .ccfffccccfffcc.
-    ..cccccccccccc..
-    `, SpriteKind.Virtical)
-Limo_V3.setPosition(88, 40)
-let Limo_H3 = sprites.create(img`
-    ..ccccccccccccccccccccccccccccccccfccccccccc....
-    .ccffffffffffffcfffffffffffffffffcccccccccccc...
-    ccccccffffffffcffffffffffffffffcccfccccccccccc..
-    cffffcccffffffcfffccccccccccccccffffcccccccccf..
-    cffffffcccccccccccccccccccccccffffffcccccccccff.
-    cfffffffcccccccccccccccccccccffffffffcccccccccf.
-    ccfffffcccccccccccccccccccccccfffffffcccccccccc.
-    ccfffffcccccccccccccccccccccccfffffffcccccccccc.
-    ccfffffcccccccccccccccccccccccfffffffccccccccccb
-    ccfffffcccccccccccccccccccccccfffffffcccccccccc.
-    cfffffffcccccccccccccccccccccffffffffcccccccccf.
-    cffffffcccccccccccccccccccccccffffffcccccccccff.
-    cffffcccffffffcfffccccccccccccccffffcccccccccf..
-    ccccccffffffffcffffffffffffffffcccfccccccccccc..
-    .ccffffffffffffcfffffffffffffffffcccccccccccc...
-    ..ccccccccccccccccccccccccccccccccfccccccccc....
-    `, SpriteKind.Horizontal)
-Limo_H3.setPosition(56, 40)
+Create_Charecters()
 V_list = sprites.allOfKind(SpriteKind.Virtical)
 H_list = sprites.allOfKind(SpriteKind.Horizontal)
 for (let V_sprite4 of V_list) {
@@ -221,10 +481,34 @@ for (let V_sprite4 of V_list) {
 for (let H_sprite4 of H_list) {
     H_sprite4.setFlag(SpriteFlag.StayInScreen, true)
 }
+Put_in_level(Driver, 72, 64)
+Put_in_level(TruckH3, 56, 40)
+Put_in_level(Limo_V3, 88, 40)
+Make_Invisible(JeepV2)
+Make_Invisible(JeepH2)
+Make_Invisible(TruckV3)
+Make_Invisible(PartyBusH3)
+Make_Invisible(TaxiH2)
+Make_Invisible(TaxiV2)
+Make_Invisible(CarH2)
+Make_Invisible(CarV2)
 game.onUpdate(function () {
     if (Driver.y == 16) {
-        timer.after(400, function () {
-            game.over(true)
-        })
+        Level += 1
+        if (Level == 1) {
+            Put_in_level(Driver, 72, 64)
+            Put_in_level(TruckH3, 56, 40)
+            Put_in_level(Limo_V3, 88, 40)
+            Make_Invisible(JeepV2)
+            Make_Invisible(JeepH2)
+            Make_Invisible(TruckV3)
+            Make_Invisible(PartyBusH3)
+            Make_Invisible(TaxiH2)
+            Make_Invisible(TaxiV2)
+            Make_Invisible(CarH2)
+            Make_Invisible(CarV2)
+        } else if (Level == 2) {
+        	
+        }
     }
 })
